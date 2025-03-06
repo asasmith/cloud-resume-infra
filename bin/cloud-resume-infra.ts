7add558c9042b1dc08dbceafc17c67690115d00a
asasmith <smith.asa.la@gmail.com>
2025-03-06 11:02:25 -0500

    move s3 code to it's own file

--- bin/cloud-resume-infra.ts
+++ bin/cloud-resume-infra.ts
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CloudResumeInfraStack } from '../lib/cloud-resume-infra-stack';
+import { S3Stack } from "../lib";
 
 const app = new cdk.App();
-new CloudResumeInfraStack(app, 'CloudResumeInfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new S3Stack(app, 'S3Stack'); 
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+app.synth();
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
