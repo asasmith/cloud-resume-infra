714b224bffe4f29c7ebade1e8f75b75cc1a2671a
asasmith <smith.asa.la@gmail.com>
2025-03-06 08:20:40 -0500

    add website bucket

---
+++ lib/cloud-resume-infra-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+export class CloudResumeInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CloudResumeInfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    new s3.Bucket(this, "CloudResumeInfraBucket", {
+      bucketName: `cloud-resume-${this.account}-${this.region}`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
